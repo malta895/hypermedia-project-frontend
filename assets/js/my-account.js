5bb81814ef03e7cee8a22f68f8ed7d0f24e7ef8c
Luca Maltagliati <malta95@hotmail.it>
2019-06-29 22:21:57 +0200

    Fixes and added address in user details

--- assets/js/my-account.js
+++ assets/js/my-account.js
@@ -6,9 +6,22 @@ $(document).ready(function () {
         $('#surname').html(data.surname);
         $('#username').html(data.username);
         $('#email').html(data.email);
-        $('#birth_data').html(data.birth_date);
-        $('#birth_data').html(data.birth_date);
-
+        $('#bday').html(new Date(data.birth_date).toLocaleDateString());
+        if(data.address){
+            let a = data.address;
+            $('#addr-first-name').html(a.first_name);
+            $('#addr-last-name').html(a.last_name);
+            $('#addr-complete-name').html(`${a.first_name} ${a.last_name}`);
+            $('#addr-street-line1').html(a.addressStreetLine1);
+            $('#addr-street-line2').html(a.addressStreetLine2);
+            $('#addr-city').html(a.city);
+            $('#addr-zip-code').html(a.zip_code);
+            $('#addr-province').html(a.province);
+            $('#addr-country').html(a.country);
+            $('.address-box ul').removeClass('hidden');
+        } else {
+            $('.address-box a').removeClass('hidden');
+        }
     });
 
 
