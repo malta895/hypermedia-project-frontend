8fcd9bd2b0b69f0bc4e0ab64c9b4a19af2f9e83c
turing95 <marco1491995@gmail.com>
2019-06-29 15:01:58 +0200

    togli libri metti libri togli libri metti libri

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         var cart = []
         for (i = 0; i < data[0].books.length; i++) {
             book = data[0].books[i].book;
-            cart.push(book)
+            cart.push(data[0].books[i])
             var id = book.book_id;
             var title = book.title;
             var authors = book.authors;
@@ -18,7 +18,7 @@ $(document).ready(function () {
             elem += '<tr> <td class="col-xs-1"><img src="' + picture + '" alt="" class="img-responsive">';
             elem += '</td> <td class="col-xs-4 col-md-5"><h4><a href="single-product.html?id='+id+'">'+title+'</a><small>M, Black, Esprit</small>';
             elem += '</h4></td> <td class="col-xs-2 text-center"><span>&euro;'+price+'</span></td>';
-            elem += '<td class="col-xs-2 col-md-1"><div class="form-group"><input type="text" class="form-control" value="'+quantity+'">';
+            elem += '<td class="col-xs-2 col-md-1"><div class="form-group"><input type="text" id='+id+' class="form-control upQuant" value="'+quantity+'">';
             elem += '</div></td> <td class="col-xs-2 text-center"><span><b>&euro;'+singleTot+'</b></span></td>';
             elem += '<td class="col-xs-1 text-center"><a href="" class="btn btn-primary">';
             elem += '<i class="fa fa-times"></i></a></td></tr>';
@@ -40,6 +40,44 @@ $(document).ready(function () {
         e.preventDefault()
         cart = JSON.parse(sessionStorage.getItem('cart'))
         console.log(cart)
+        $('#cart .upQuant').each(function (index) {
+            console.log($(this).attr('id'))
+            for (i = 0; i < cart.length; i++) {
+                
+                idN = $(this).attr('id')
+                qN = $(this).val()
+                idO = cart[i].book.book_id
+                qO = cart[i].quantity
+                if (idO == idN) {
+                    
+                    
+                    if (qN === qO) {
+                        break;
+                    } else if (qN > qO) {
+                        let add = qN - qO;
+                        $.ajax({
+                            url: '/api/cart/add/book/' + idN+'?quantity='+add,
+                            type: 'PUT',
+                            success: function (response) {
+
+                            }
+                        });
+
+                    } else if (qN < qO) {
+                        let del = qO-qN;
+                        $.ajax({
+                            url: '/api/cart/remove/book/' + idN + '?quantity=' + del,
+                            type: 'DELETE',
+                            success: function (response) {
+
+                            }
+                        });
+
+                    }
+                }
+            }
+
+        });
         
 
     });
