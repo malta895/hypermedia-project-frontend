102e9e6449db631f8083c9a3461122ed4596d539
turing95 <marco1491995@gmail.com>
2019-06-30 01:19:45 +0200

    almost done the order!!! should FIX  dealing with ENUM in shipment and payment.

--- assets/js/checkout-review.js
+++ assets/js/checkout-review.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
     $.getJSON('/api/cart', function (data) {  // /api/cart GET CART
-
+        var order = JSON.parse(sessionStorage.getItem('order'))
+        $('.name').html(order.first_name + ' ' + order.last_name)
+        $('.address').html(order.addressStreetLine1)
+        $('.zipcity').html(order.zip_code + ' ' + order.city)
+        $('.country').html(order.country)
+        $('.payment_method').html(order.payment_method)
+        $('.shipping_method').html(order.shipping_method)
         total = 0
         for (i = 0; i < data[0].books.length; i++) {
             book = data[0].books[i].book;
@@ -24,6 +30,26 @@ $(document).ready(function () {
         }
 
         $(".total").html('&euro;' + total);
+        $('.checkout-steps form').on('submit', function (e) {
+            e.preventDefault()
+            var formData = new FormData();
+            formData.append('first_name', order.first_name);
+            formData.append('last_name', order.last_name);
+            formData.append('addressStreetLine1', order.addressStreetLine1);
+            formData.append('city', order.city);
+            formData.append('zip_code ', order.zip_code );
+            formData.append('province', order.state);
+            formData.append('country', order.country);
+            formData.append('payment_method', order.payment_method);
+            formData.append('shipping_method', order.shipping_method);
+            order.province=order.state
+            console.log(order)
+            console.log(formData)
+            //formData=formData.serialize(); 
+            $.post('/api/orders/create', order, function (data) { }, 'application/x-www-form-urlencoded');
+
+
+        });
 
 
 
