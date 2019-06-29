418ba37d73b64f37a3587080a6dc23aa7b53875c
turing95 <marco1491995@gmail.com>
2019-06-29 11:18:59 +0200

    varie review a cart su single product

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -17,16 +17,16 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
-
+var id = getUrlParameter('id');
 $(window).on("load",function () {
-    var id = getUrlParameter('id');
+    
     $.getJSON('/api/books/'+id, function (data) {  // GET BOOK BY ID /api/book/{bookId}    
         console.log(data);
         for (i = 0; i < data.length; i++) {
             var id = data[i].book_id;
             var title = data[i].title;
             var isbn = data[i].isbn;
-
+            var rating = data[i].average_rating
             var authors = data[i].authors;
             a=''
             for (y = 0; y < authors.length; y++) {
@@ -39,8 +39,17 @@ $(window).on("load",function () {
             for (k = 0; k < genre.length; k++) {
                 g+=genre[k]
             }
+            d = '';
+            for (i = 1; i <= 5; i++) {
+                if (i <= rating) {
+                    d += '<i class="fa fa-star"></i>';
+                } else {
+                    d += '<i class="fa fa-star-o"></i>'
+                }
+
+            }
             
-            var elem = '';
+            $(".product-rating").html(d);
             $("#title").html(title);
             $("#author").html("<span>Author</span>"+a);
             $("#price").html(price +'\u20AC');
@@ -54,7 +63,7 @@ $(window).on("load",function () {
                     url: '/api/cart/add/book/' + id,
                     type: 'PUT',
                     success: function (response) {
-                        //...
+                        addCart()
                     }
                 });
             });
@@ -146,11 +155,11 @@ $(window).on("load",function () {
 
 
         let formData = $(this).serialize();
-        $.post('/api/books/'+id+'/reviews/add', formData, function (res) {
+        $.post('/api/books/' + id + '/reviews/add', formData, function (res) {
 
-            console.log('SUCCESS');
-
-            console.log(res);
+            addReview()
+            $('.modal').modal('hide');
+            
 
             
 
@@ -179,3 +188,166 @@ $(window).on("load",function () {
     });
 
 });
+
+function addReview() {
+    $.getJSON('/api/books/' + id, function (data) {  // GET BOOK BY ID /api/book/{bookId}    
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+            var id = data[i].book_id;
+            var title = data[i].title;
+            var isbn = data[i].isbn;
+            var rating = data[i].average_rating
+            var authors = data[i].authors;
+            a = ''
+            for (y = 0; y < authors.length; y++) {
+                a += authors[y].name
+            }
+            var price = data[i].price;
+            var picture = data[i].picture;
+            var genre = data[i].genres;
+            g = ''
+            for (k = 0; k < genre.length; k++) {
+                g += genre[k]
+            }
+            d = '';
+            for (i = 1; i <= 5; i++) {
+                if (i <= rating) {
+                    d += '<i class="fa fa-star"></i>';
+                } else {
+                    d += '<i class="fa fa-star-o"></i>'
+                }
+
+            }
+
+            $(".product-rating").html(d);
+            $("#title").html(title);
+            $("#author").html("<span>Author</span>" + a);
+            $("#price").html(price + '\u20AC');
+            $("#isbn").html("<span>ISBN</span>" + isbn);
+            $("#genre").html("<span>Genre</span>" + genre);
+            var img = '<div class="item"><img src="' + picture + '" id="img" class="img - responsive" alt=""></div>';
+            $("#product-carousel").html(img);
+            
+
+        }
+
+
+
+
+
+
+
+    });
+    //get review by book id
+    $.getJSON('/api/books/' + id + '/reviews', function (data) { //get similar_books by book_id   
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+            var review = data[i];
+            var id = data[i].review_id;
+            var title = data[i].title;
+            var book = data[i].book;
+            var user = data[i].user;
+            var rating = data[i].rating;
+            var elem = '';
+            var d = '';
+            for (i = 1; i <= 5; i++) {
+                if (i <= rating) {
+                    d += '<i class="fa fa-star"></i>'
+                } else {
+                    d += '<i class="fa fa-star-o"></i>'
+                }
+
+            }
+            elem += '<div class="media">';
+            elem += '<div class="media-body"> <h3 class="media-heading">' + d + ' ' + title + '</h3>';
+            elem += '<div class="meta"> <span class="date">' + review.date_published + '</span></div>';
+            elem += '<p>' + review.text + '</p> </div> </div>';
+            $(".comments").append(elem);
+        }
+
+
+
+
+
+
+
+    });
+    $.getJSON('/api/books/' + id + '/related', function (data) { //get similar_books by book_id   
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+            var id = data[i].book_id;
+            var title = data[i].title;
+            var authors = data[i].authors;
+            var price = data[i].price;
+            var picture = data[i].picture;
+            var genre = data[i].genre;
+            var elem = '';
+            elem += '<div class="col-sm-3 col-xs-6" id="' + id + '">';
+            elem += '<article class="product-item">';
+            elem += '<div class="row">';
+            elem += '<div class="col-sm-3">';
+            elem += '<div class="product-overlay">';
+            elem += '<div class="product-mask"></div>';
+            elem += '<a href="pages/single-product.html?id=' + id + '" class="product-permalink"></a><img src="' + picture + '" width="262.5" height="350" class="img-responsive" alt="">';
+            elem += '<img src="' + picture + '" class="img-responsive product-image-2" alt="" width="262.5" height="350"></div></div>';
+            elem += '<div class="col-sm-9"><div class="product-body">';
+            elem += '<h3>' + title + '</h3>';
+            elem += '<div class="product-rating"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-o"></i><i class="fa fa-star-o"></i></div>';
+            elem += '<span class="price"><ins><span class="amount">' + currencies.EUR + price + '</span></ins></span>';
+            elem += '<div class="buttons"><a href="" class="btn btn-primary btn-sm add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a><a href="" class="btn btn-primary btn-sm"><i class="fa fa-heart"></i></a></div>';
+            elem += '</div>';
+            elem += '</div>';
+            elem += '</div>';
+            elem += '</article';
+            elem += '</div>';
+            $("#products").append(elem);
+        }
+
+
+
+
+
+
+
+    });
+}
+function addCart() {
+
+    $.getJSON('/api/cart', function (data) {
+        console.log(data)// /api/cart GET CART
+        $(".dropdown-menu").empty();
+        for (i = 0; i < data[0].books.length; i++) {
+            console.log(data[0].books[i])
+            book = data[0].books[i].book;
+            var id = book.book_id;
+            var title = book.title;
+            var authors = book.authors;
+            var price = book.price;
+            var picture = book.picture;
+            var genre = book.genres;
+            var quantity = data[0].books[i].quantity;
+            var elem = '';
+            elem += '<li><div class="row"><div class="col-sm-3">';
+            elem += '<img src="' + picture + '" class="img-responsive" alt="">';
+            elem += '</div><div class="col-sm-9">';
+            elem += '<h4><a href="single-product.html?id=' + id + '">Fusce Aliquam</a></h4>';
+            elem += '<p>' + quantity + 'x - &euro;' + price + '</p>';
+            elem += '<a href="#" class="remove"><i class="fa fa-times-circle"></i></a>';
+            elem += '</div></div></li>';
+
+
+            $(".dropdown-menu").append(elem);
+
+        }
+        elem = '<li> <div class="row"> <div class="col-sm-6"> <a href="cart.html" class="btn btn-primary btn-block">View Cart</a> </div> <div class="col-sm-6"> <a href="checkout.html" class="btn btn-primary btn-block">Checkout</a> </div> </div> </li>';
+        $(".dropdown-menu").append(elem);
+
+
+
+
+
+
+
+
+    });
+}
