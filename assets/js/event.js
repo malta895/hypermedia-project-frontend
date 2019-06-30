a0a2a8efe0affec70ef70661f089901c6ad76093
Luca Maltagliati <malta95@hotmail.it>
2019-06-30 19:58:58 +0200

    Fixes to Event

--- assets/js/event.js
+++ assets/js/event.js
@@ -1,34 +1,29 @@
 $(document).ready(function () {
     $.getJSON('/api/events', function (data) {  // /api/books GET ALL EVENTS
 
-        for (i = 0; i < data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
             var id = data[i].event_id;
             var date = data[i].date;
             var location = data[i].location;
-            //var address = location.street_line1 + ','+location.street_line2 +','+ location.city;
-            /*var book = data[i].presented_book;
+
+            var book = data[i].presented_book;
             var title = book.title;
             var picture = book.picture_path;
-            var authors = book.authors[0];
-            var genre = book.genre;*/
-            var title = 'prova';
-            var authors = 'prova';
-            var genre = 'prova';
+            var authors = book.authors.reduce(function(acc, currVal){
+                return acc === '' ? currVal : ', ' + currVal;
+            }, '');
+            var genre = book.genre.name;
             var elem = '';
             elem += '<div class="col-sm-12 col-xs-6"> <article class="product-item"> <div class="row"> <div class="col-sm-3"> <div class="product-overlay"> <div class="product-mask"></div>';
             elem += '<a href="single-event.html?id=' + id + '" class="product-permalink"></a>';
             elem += '<img src="" class="img-responsive" alt="">';
             elem += ' </div> </div> <div class="col-sm-9"> <div class="product-body">';
-            elem += '<h3>Presentation:'+title+' (Author: '+authors+')</h3> <h4>Date:</h4> <p>'+date+'</p> <h4>Where:</h4> <p>'+address+'</p> <h4>Plot:</h4> <p>Mini Plot</p>';
+            elem += '<h3>Presentation:'+title+' (Authors: '+authors+')</h3> <h4>Date:</h4> <p>'+date+'</p> <h4>Where:</h4> <p>'+address+'</p> <h4>Plot:</h4> <p>Mini Plot</p>';
             elem += ' </div> </div> </div> </article> </div>';
+
             $("#events").append(elem);
 
         }
-
-
-
-
-
 
     });
 });
