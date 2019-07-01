e5eff58f13cb9a160859c2acea1492130565e2c7
Luca Maltagliati <malta95@hotmail.it>
2019-07-01 12:09:27 +0200

    Cart definitivo

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,86 +1,75 @@
-$(document).ready(function () {
-    $.getJSON('/api/cart', function (data) {  // /api/cart GET CART
-             
-        total = 0
-        var cart = []
-        for (i = 0; i < data[0].books.length; i++) {
-            book = data[0].books[i].book;
-            cart.push(data[0].books[i])
+$(document).ready(function() {
+    $.getJSON('/api/cart', function(data) { // /api/cart GET CART
+
+
+        var cart = data[0];
+        let total = cart.total_amount;
+        for (let i = 0; i < cart.books.length; i++) {
+            let book = cart.books[i].book;
             var id = book.book_id;
             var title = book.title;
-            var authors = book.authors;
+            var authors = book.authors.reduce(function(acc, currVal) {
+                return (acc === '' ? '' : (acc + ', ')) + currVal.name;
+            }, '');
             var price = book.price;
             var picture = book.picture;
             var genre = book.genres;
-            var quantity = data[0].books[i].quantity;
+            var quantity = cart.books[i].quantity;
             var singleTot = price * quantity;
             var elem = '';
             elem += '<tr> <td class="col-xs-1"><img src="' + picture + '" alt="" class="img-responsive">';
-            elem += '</td> <td class="col-xs-4 col-md-5"><h4><a href="single-product.html?id='+id+'">'+title+'</a><small>M, Black, Esprit</small>';
-            elem += '</h4></td> <td class="col-xs-2 text-center"><span>&euro;'+price+'</span></td>';
-            elem += '<td class="col-xs-2 col-md-1"><div class="form-group"><input type="text" id='+id+' class="form-control upQuant" value="'+quantity+'">';
-            elem += '</div></td> <td class="col-xs-2 text-center"><span><b>&euro;'+singleTot+'</b></span></td>';
+            elem += '</td> <td class="col-xs-4 col-md-5"><h4><a href="single-product.html?id=' + id + '">' + title + '</a><small>' + authors + '</small>';
+            elem += '</h4></td> <td class="col-xs-2 text-center"><span>&euro;<span class="item-price">' + price + '</span></span></td>';
+            elem += '<td class="col-xs-2 col-md-1"><div class="form-group"><input type="number" id=' + id + ' class="form-control upQuant" value="' + quantity + '">';
+            elem += '</div></td> <td class="col-xs-2 text-center"><span><b>&euro;<span class="single-total">' + singleTot + '</span></b></span></td>';
             elem += '<td class="col-xs-1 text-center"><a href="" class="btn btn-primary">';
             elem += '<i class="fa fa-times"></i></a></td></tr>';
-            total+=singleTot
+
+
             $("#cart").append(elem);
 
         }
-        cart = JSON.stringify(cart)
-        sessionStorage.setItem('cart', cart)
-        $(".total").html('&euro;'+total);
-        
+        $("#total").html('&euro;' + total);
 
+        $('.upQuant').change(function(e) {
+            //e.prevent Default()
+            console.log($(this).attr('id'));
 
+            let newQuantity = $(this).val();
+            let bookId = $(this).attr('id');
 
+            let outerColumn = $(this).parents('tr');
+            let unitPrice = outerColumn.find('.item-price');
 
+            $.ajax({
+                url: '/api/cart/setQuantity/book/' + bookId + '?quantity=' + newQuantity,
+                type: 'PUT',
+                success: function(response) {
+                    if(newQuantity <= 0)
+                        outerColumn.remove();
+                    total = 0;
+                    let rows = $('#cart tr').each(function(){
+                        console.log(this);
+                        let q = $(this).find('.upQuant').val();
+                        let p = $(this).find('.item-price').html();
+                        let singleTotalPrice = q*p;
+                        $(this).find('.single-total').html(singleTotalPrice);
+                        total += singleTotalPrice;
+                    });
 
-    });
-    $('.update-cart').on('click', function (e) {
-        //e.preventDefault()
-        cart = JSON.parse(sessionStorage.getItem('cart'))
-        console.log(cart)
-        $('#cart .upQuant').each(function (index) {
-            console.log($(this).attr('id'))
-            for (i = 0; i < cart.length; i++) {
-                
-                idN = $(this).attr('id')
-                qN = $(this).val()
-                idO = cart[i].book.book_id
-                qO = cart[i].quantity
-                if (idO == idN) {
-                    
-                    
-                    if (qN === qO) {
-                        break;
-                    } else if (qN > qO) {
-                        let add = qN - qO;
-                        $.ajax({
-                            url: '/api/cart/add/book/' + idN+'?quantity='+add,
-                            type: 'PUT',
-                            success: function (response) {
+                    $('#total').html('&euro;' + total);
 
-                            }
-                        });
-
-                    } else if (qN < qO) {
-                        let del = qO-qN;
-                        $.ajax({
-                            url: '/api/cart/remove/book/' + idN + '?quantity=' + del,
-                            type: 'DELETE',
-                            success: function (response) {
-
-                            }
-                        });
-
-                    }
                 }
-            }
+            })
+                .fail(function(){
+
+                });
+
 
         });
-        
+
 
     });
 
-});
 
+});
