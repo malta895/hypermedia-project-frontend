84d09fc740c1d3cc18986a2edd12d3be8358ed40
turing95 <marco1491995@gmail.com>
2019-07-02 17:27:37 +0200

    fix multi pages

--- assets/js/multi-pages.js
+++ assets/js/multi-pages.js
@@ -20,33 +20,37 @@ $(document).ready(function () {
         $('.myaccount').html(data.first_name + ' ' + data.surname);
         sessionStorage.setItem('user', JSON.stringify(data));
         $.getJSON('/api/cart', function (data) {
-            console.log(data)// /api/cart GET CART
+            
             $(".navbar-cart > ul").empty();
-            for (i = 0; i < data[0].books.length; i++) {
-                console.log(data[0].books[i])                
-                book = data[0].books[i].book;
-                var id = book.book_id;
-                var title = book.title;
-                var authors = book.authors;
-                var price = book.price;
-                var picture = book.picture;
-                var genre = book.genres;
-                var quantity = data[0].books[i].quantity;
-                var elem = '';
-                elem += '<li><div class="row"><div class="col-sm-3">';
-                elem += '<img src="'+picture+'" class="img-responsive" alt="">';
-                elem += '</div><div class="col-sm-9">';
-                elem += '<h4><a href="single-product.html?id='+id+'">'+title+'</a></h4>';
-                elem += '<p>' + quantity +'x - &euro;'+price+'</p>';
-                elem += '<a href="#" class="remove"><i class="fa fa-times-circle"></i></a>';
-                elem += '</div></div></li>';                
+            if (data.length > 0) {
+                for (i = 0; i < data[0].books.length; i++) {
+                    console.log(data[0].books[i])
+
+                    book = data[0].books[i].book;
+                    var id = book.book_id;
+                    var title = book.title;
+                    var authors = book.authors;
+                    var price = book.price;
+                    var picture = book.picture;
+                    var genre = book.genres;
+                    var quantity = data[0].books[i].quantity;
+                    var elem = '';
+                    elem += '<li><div class="row"><div class="col-sm-3">';
+                    elem += '<img src="' + picture + '" class="img-responsive" alt="">';
+                    elem += '</div><div class="col-sm-9">';
+                    elem += '<h4><a href="single-product.html?id=' + id + '">' + title + '</a></h4>';
+                    elem += '<p>' + quantity + 'x - &euro;' + price + '</p>';
+                    elem += '<a href="#" class="remove"><i class="fa fa-times-circle"></i></a>';
+                    elem += '</div></div></li>';
 
 
+                    $(".navbar-cart > ul").append(elem);
+
+                }
+                elem = '<li> <div class="row"> <div class="col-sm-6"> <a href="cart.html" class="btn btn-primary btn-block">View Cart</a> </div> <div class="col-sm-6"> <a href="checkout.html" class="btn btn-primary btn-block">Checkout</a> </div> </div> </li>';
                 $(".navbar-cart > ul").append(elem);
-
             }
-            elem = '<li> <div class="row"> <div class="col-sm-6"> <a href="cart.html" class="btn btn-primary btn-block">View Cart</a> </div> <div class="col-sm-6"> <a href="checkout.html" class="btn btn-primary btn-block">Checkout</a> </div> </div> </li>';
-            $(".navbar-cart > ul").append(elem);
+            
 
             
 
