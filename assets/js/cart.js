5c2881ffc2b0f1967a92147b05e25a9b62f86c70
Luca Maltagliati <malta95@hotmail.it>
2019-07-02 11:34:57 +0200

    Cart finished

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,6 +1,50 @@
 $(document).ready(function() {
+
+    $('#button-empty-cart').click(function(e){
+        e.preventDefault();
+
+        $('#cart-modal-text').html("Are you sure you want to remove all items from the cart?");
+
+        $('#modal-confirm-delete-cart').modal();
+
+
+        $('#modal-confirm-delete-cart .modal-footer .btn-primary').off();
+
+        $('#modal-confirm-delete-cart .modal-footer .btn-primary').click(function(){
+            $.ajax({
+                url: '/api/cart/empty',
+                type: 'DELETE',
+                statusCode: {
+                    200: function(response) {
+                        $('#cart tr').remove();
+                        $('#empty-cart').removeClass('hidden');
+                        $('.shopping-cart-form').addClass('hidden');
+                    }
+                }
+            })
+                .fail(function(response){
+                    if(response.status !== 200){
+                        $('.modal').modal('hide');
+                        setTimeout(function(){$('#modal-server-error').modal();}, 200);
+                    }
+                });
+        });
+
+
+
+
+
+
+    });
+
+
+
     $.getJSON('/api/cart', function(data) { // /api/cart GET CART
 
+        if(data.length === 0){
+            $('#empty-cart').removeClass('hidden');
+            return;
+        }
 
         var cart = data[0];
         let total = cart.total_amount;
@@ -17,18 +61,62 @@ $(document).ready(function() {
             var quantity = cart.books[i].quantity;
             var singleTot = price * quantity;
             var elem = '';
+
             elem += '<tr> <td class="col-xs-1"><img src="' + picture + '" alt="" class="img-responsive">';
             elem += '</td> <td class="col-xs-4 col-md-5"><h4><a href="single-product.html?id=' + id + '">' + title + '</a><small>' + authors + '</small>';
             elem += '</h4></td> <td class="col-xs-2 text-center"><span>&euro;<span class="item-price">' + price + '</span></span></td>';
             elem += '<td class="col-xs-2 col-md-1"><div class="form-group"><input type="number" id=' + id + ' class="form-control upQuant" value="' + quantity + '">';
             elem += '</div></td> <td class="col-xs-2 text-center"><span><b>&euro;<span class="single-total">' + singleTot + '</span></b></span></td>';
-            elem += '<td class="col-xs-1 text-center"><a href="" class="btn btn-primary">';
-            elem += '<i class="fa fa-times"></i></a></td></tr>';
+            elem += '<td class="col-xs-1 text-center"><button class="btn btn-primary" data-toggle="modal" data-target="#modal-confirm-delete-cart" id="button' + id + '">';
+            elem += '<i class="fa fa-times"></i></button></td></tr>';
 
 
             $("#cart").append(elem);
 
+            $("#button" + id).click(function(e){
+                e.preventDefault();
+                let bookId = $(this).attr('id').slice(6);
+
+                let outerColumn = $(this).parents('tr');
+                $('#cart-modal-text').html(`Do you really confirm to remove all copies of <i>${outerColumn.find('h4 a').html()}</i> from the cart?`);
+                $('#modal-confirm-delete-cart .modal-footer .btn-primary').off('click');
+                $('#modal-confirm-delete-cart .modal-footer .btn-primary').click(function(){
+                    $.ajax({
+                        url: '/api/cart/setQuantity/book/' + bookId + '?quantity=0',
+                        type: 'PUT',
+                        success: function(response) {
+                            outerColumn.remove();
+                            let total = 0;
+                            let rows = $('#cart tr').each(function(){
+                                console.log(this);
+                                let q = $(this).find('.upQuant').val();
+                                let p = $(this).find('.item-price').html();
+                                let singleTotalPrice = q*p;
+                                $(this).find('.single-total').html(singleTotalPrice);
+                                total += singleTotalPrice;
+                            });
+
+                            $('#total').html('&euro;' + total);
+                        }
+                    })
+                        .fail(function(){
+                            $('.modal').modal('hide');
+                            setTimeout(function(){$('#modal-server-error').modal();}, 200);
+                        });
+                });
+            });
+
         }
+
+        if(data.length !== 0){
+            $('#empty-cart').addClass('hidden');
+            $('.shopping-cart-form').removeClass('hidden');
+        } else {
+            $('#empty-cart').removeClass('hidden');
+        }
+
+
+
         $("#total").html('&euro;' + total);
 
         $('.upQuant').change(function(e) {
@@ -62,9 +150,8 @@ $(document).ready(function() {
                 }
             })
                 .fail(function(){
-
+                    $('#modal-server-error').modal();
                 });
-
 
         });
 
